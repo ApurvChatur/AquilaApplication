d6f912d80bbe678892aa17e16a27ceec7c1f5e33
ApurvChatur <chaturapurv@gmail.com>
2024-06-06 15:56:01 +0530

    Hello

--- site/frontend/src/love/cComponent/gSidebarComponent/children/aChatPageComponent/index.jsx
+++ site/frontend/src/love/cComponent/gSidebarComponent/children/aChatPageComponent/index.jsx
@@ -91,15 +91,15 @@ const ChatPageComponent = ({ Redux, ReduxUltimate, APICalls, Function, socket })
   useSocketEvent(socket, eventHandler)
   
   return (
-    <div className="bg-[#ADEFD1] dark:bg-[#00203F] text-[#00203F] dark:text-[#ADEFD1] ">
+    <div className="bg-[#F2AA4C] dark:bg-[#101820] text-[#101820] dark:text-[#F2AA4C] ">
       <Suspense fallback={<Loader text="Suspense Boom Loading..." />}>
 
         <div 
           className="
             border flex-col md:flex
-            bg-[#ADEFD1] dark:bg-[#00203F]
-            border-[#00203F] dark:border-[#ADEFD1]
-            text-[#00203F] dark:text-[#ADEFD1]
+            bg-[#F2AA4C] dark:bg-[#101820]
+            border-[#101820] dark:border-[#F2AA4C]
+            text-[#101820] dark:text-[#F2AA4C]
           "
         >
           <TooltipProvider delayDuration={0}>
@@ -336,7 +336,7 @@ const ChatPageComponent = ({ Redux, ReduxUltimate, APICalls, Function, socket })
                 </div>
 
                 {Redux.state.RequiredObject?.Loading ? "Loading..." : null}
-                <Separator className="bg-[#00203F] dark:bg-[#ADEFD1]" />
+                <Separator className="bg-[#101820] dark:bg-[#F2AA4C]" />
 
                 <ScrollArea className="h-full rounded-md">
                   <ChatListComponent
@@ -535,7 +535,7 @@ const ChatPageComponent = ({ Redux, ReduxUltimate, APICalls, Function, socket })
                       </div>
                     </div>
 
-                    <Separator className="bg-[#00203F] dark:bg-[#ADEFD1]" />
+                    <Separator className="bg-[#101820] dark:bg-[#F2AA4C]" />
                   
                     <ChatMessageRetrieveComponent Redux={Redux} ReduxUltimate={ReduxUltimate} socket={socket} APICalls={APICalls} />
                   </React.Fragment>
